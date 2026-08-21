f0ca4930e0eec34dd0a39d4bc957230c08f63179
Emre Tüfekcioglu <emre_toglu@hotmail.com>
2026-08-21 13:47:20 +0200

    first commit

---
+++ DeepDive.js
@@ -0,0 +1,14 @@
+function ScrollToUdstyr() {
+    const udstyrSection = document.getElementById('udstyrBaggrund');
+    udstyrSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function ScrollToKurser() {
+    const kurserSection = document.getElementById('kurserBaggrund');
+    kurserSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function ScrollToKontakt() {
+    const kontaktSection = document.getElementById('kontakt');
+    kontaktSection.scrollIntoView({ behavior: 'smooth' });
+}
